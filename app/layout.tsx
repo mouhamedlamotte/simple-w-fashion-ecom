718890c81fe1fc37bf28075720d01b73f92836ed
mouhamedlamotte <mouhamedlamotte.dev@gmail.com>
2025-10-17 01:40:40 +0000

    fix : edit title

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Ecomerce App',
+  title: 'Women fashion show',
 }
 
 export default function RootLayout({
